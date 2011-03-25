db3a3376cdc82b4c6a930abef1a191be4930e966
Matthew Kitt <mk.kitt@gmail.com>
2011-03-24 20:37:23 -0600

    Visual example

---
+++ examples/example.js
@@ -0,0 +1,16 @@
+
+(function() {
+  var array_suite = new Benchmark.Suite('Array');
+  var array_reporter = new Benchmark.Reporter(array_suite, 'log', true);
+
+  array_suite.add('Array with brackets', function() {
+    var a = [];
+  });
+
+  array_suite.add('Array with new', function() {
+    var a = new Array();
+  });
+
+  array_suite.run(true);
+}());
+
