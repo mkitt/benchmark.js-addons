c8c6d5d2312fc2fb81dfb8169366d038ef469b7e
Matthew Kitt <mk.kitt@gmail.com>
2011-03-29 19:21:07 -0600

    Adding the test cases to the demos.
    These are really gnarly and will be cleaned up later.

--- examples/dom-demo.js
+++ examples/dom-demo.js
@@ -2,6 +2,9 @@
 (function() {
   var array_suite = new Benchmark.Suite('Array');
   var array_reporter = new Benchmark.Reporter(array_suite);
+  var cond_suite = new Benchmark.Suite('Conditionals');
+  var cond_reporter = new Benchmark.Reporter(cond_suite);
+  var cases = new Benchmark.TestCase([array_suite, cond_suite]);
 
   array_suite.add('Array with brackets', function() {
     var a = [];
@@ -11,6 +14,30 @@
     var a = new Array();
   });
 
-  array_suite.run(true);
+  cond_suite.add('if/elseif/else', function() {
+    var condition = 'end';
+    if (condition === 'start')
+      condition = 'start';
+    else if (condition === 'middle')
+      condition = 'middle';
+    else
+      condition = 'end';
+  });
+
+  cond_suite.add('switch', function() {
+    var condition = 'end';
+    switch (condition) {
+      case 'start':
+        condition = 'start';
+      break;
+      case 'middle':
+        condition = 'middle';
+      break;
+      default:
+        condition = 'end';
+      break;
+    }
+  });
+  cases.run();
 }());
 
--- examples/node-demo.js
+++ examples/node-demo.js
@@ -4,9 +4,13 @@
   require.paths.push('./src');
   var Benchmark = require('benchmark');
   var Reporter = require('benchmark-reporter');
+  var TestCase = require('benchmark-testcase');
 
   var array_suite = new Benchmark.Suite('Array');
   var array_reporter = new Benchmark.Reporter(array_suite);
+  var cond_suite = new Benchmark.Suite('Conditionals');
+  var cond_reporter = new Benchmark.Reporter(cond_suite);
+  var cases = new Benchmark.TestCase([array_suite, cond_suite]);
 
 
   array_suite.add('Array with brackets', function() {
@@ -17,6 +21,30 @@
     var a = new Array();
   });
 
-  array_suite.run(true);
+  cond_suite.add('if/elseif/else', function() {
+    var condition = 'end';
+    if (condition === 'start')
+      condition = 'start';
+    else if (condition === 'middle')
+      condition = 'middle';
+    else
+      condition = 'end';
+  });
+
+  cond_suite.add('switch', function() {
+    var condition = 'end';
+    switch (condition) {
+      case 'start':
+        condition = 'start';
+      break;
+      case 'middle':
+        condition = 'middle';
+      break;
+      default:
+        condition = 'end';
+      break;
+    }
+  });
+  cases.run();
 }());
 
